0ab0eb85b0cbc7d2124e581f00e1f349b25b109f
Madi <110640717+braidfordm@users.noreply.github.com>
2022-09-22 11:55:58 +1200

    Add files via upload

---
+++ 00_bit_calc.py
@@ -0,0 +1,19 @@
+# Functions go here
+
+# Put series of symbols at start and end of text ( for emphasis)
+def statement_generator(text, decoration):
+
+    # Make string with five characters
+    ends = decoration * 5
+
+    # add decoration to start and end statement 
+    statement = "{} {} {}".format(ends, text, ends)
+
+    print()
+    print(statement)
+    print()
+
+    return ""
+
+# Main routine goes here
+statement_generator("look - stars", "*")
